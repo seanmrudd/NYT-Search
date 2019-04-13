98dee82238c9eb3e2d8c532c4151ea3b388f3145
Lilu <KHussell@gmail.com>
2019-04-13 12:17:26 -0400

    updated queryURL

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,6 +1,8 @@
 var subject= "climate"
-    var pubDate= "2009-04-13"
-    var queryURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + subject + "&fq=" + pubDate + "&api-key=543iW10yeJ2kpLH4K3ZVAQaVp0q25ISm"
+var limit= 10
+var beginDate= 20090413
+var endDate= 20090413
+var queryURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + subject + "&fq=" + pubDate + "&begin_date="+beginDate +"&end_date="+endDate+""+limit + "&api-key=543iW10yeJ2kpLH4K3ZVAQaVp0q25ISm"
   
   $.ajax({
       url: queryURL,
